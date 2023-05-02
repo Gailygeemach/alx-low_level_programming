3962e1ae56fef61a3c343298db5c12d05c957e0b
Gailygeemach <graciousmachuwe@gmail.com>
2023-05-02 11:52:50 -0700

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackname
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		
+
+
